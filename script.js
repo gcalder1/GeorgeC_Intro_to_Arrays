e76cd25452f25681b23069007bd8f13cd4f975b2
unknown <george.calderon2000@gmail.com>
2025-04-07 18:16:51 -0400

    Linked script file to index
    Made sure things were connected

---
+++ script.js
@@ -0,0 +1,14 @@
+//warmup
+let arr = new Array();
+let arr2 = ["hello world", 200, true];
+
+console.log(arr);
+console.log(arr2);
+
+console.log(arr2[2]);
+
+arr2[1] = 100;
+
+console.log(arr2);
+//warmup
+
