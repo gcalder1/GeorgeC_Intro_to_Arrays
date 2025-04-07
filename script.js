6c7fe4f15f353fa9fe667736bd6919773d3319e3
unknown <george.calderon2000@gmail.com>
2025-04-07 18:48:55 -0400

    warm up
    just did warm up stuff along with class

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ console.log(arr2[2]);
 arr2[1] = 100;
 
 console.log(arr2);
+console.log(arr2[1]);
+
+console.log("length of arr2:", arr2.length);
+
+console.log("highest index in arr2:", arr2.length - 1);
+
+console.log("last element in the arr2:", arr2[arr2.length - 1]);
+
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[fruits.length]); //should show as an error because there is no 3rd index
+
 //warmup
 
